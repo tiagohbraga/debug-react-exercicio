70473d9c10d81be2a2ae06e01b0353474bf40560
Tiago Braga <tiagohbraga@gmail.com>
2022-10-24 16:01:21 -0300

    solução de erros

---
+++ src/components/CardPequeno/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+
+export const LayoutCardPequeno = styled.section`
+        display: flex;
+        align-items: center;
+        border: 1px solid black;
+        padding: 10px 10px;
+`
+
+export const ImgCardPequeno = styled.img`
+    width: 35px;
+    margin-right: 10px;
+    border-radius: 50%;
+`
+
+export const InfosCardPequeno = styled.section`
+    display: flex;
+    align-items: center;
+`
+
+export const TituloCardPequeno = styled.h4`
+    margin-right: 10px;
+`
